041fc338a26bf46c9a1f85d977c528b8d2c55cf3
RajatofficialNeighbrgithub <rajatrao@neighbrsnook.com>
2024-10-16 03:26:54 +0530

    added animation

---
+++ ImageGalleryApp/ImageGalleryApp/View/ImageDetail/ImageDetailCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ImageDetailCollectionViewCell.swift
+//  ImageGalleryApp
+//
+//  Created by Mac on 16/10/24.
+//
+
+import UIKit
+
+class ImageDetailCollectionViewCell: UICollectionViewCell {
+    
+}
